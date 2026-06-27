3f71062550ee7f1507fc85766af0f77e7713f41b
edisonpedroza1991 <edisonpedroza76@gmail.com>
2026-06-27 14:21:54 +0200

    Add logging for success and error cases

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#ruta /Users/edisonpedroza/practica_linux/ejercicios1/backups_proyecto
+#ruta /Users/edisonpedroza/devops-semanal/ejercicios1/backups_proyecto
 #ruta_script /Users/edisonpedroza/devops-semanal/backup.sh
 
 FECHA=$(date +%Y-%m-%d_%H-%M-%S)
+ARCHIVO_LOG="/Users/edisonpedroza/devops-semanal/ejercicios1/backup.log"
 ARCHIVO_DESTINO="/Users/edisonpedroza/devops-semanal/ejercicios1/backups_tar_gz/respaldo_backups_$FECHA.tar.gz"
 CARPETA_DESTINO="/Users/edisonpedroza/devops-semanal/ejercicios1/backups_tar_gz"
 CARPETA_ORIGEN="/Users/edisonpedroza/devops-semanal/ejercicios1/backups_proyecto"
@@ -15,6 +16,9 @@ fi
 if   [ -d "$CARPETA_ORIGEN" ]; then
     tar -czf "$ARCHIVO_DESTINO" "$CARPETA_ORIGEN"
     echo "Backup creado en: $ARCHIVO_DESTINO"
+    echo "[ $FECHA ] backup creado: $ARCHIVO_DESTINO" >> "$ARCHIVO_LOG"
 else
     echo "ERROR: no existe la carpeta a respaldar"
+    echo "[ $FECHA ] ERROR: no existe la carpeta a respaldar" >> "$ARCHIVO_LOG"
+
 fi
